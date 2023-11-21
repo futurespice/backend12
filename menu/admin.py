cfd858bf9be1e6e026f6a257ba67829bcde0c3b5
Azat <azatss442@gmail.com>
2023-11-22 00:37:27 +0600

    new changes

--- menu/admin.py
+++ menu/admin.py
@@ -1,14 +1,7 @@
-# menu/admin.py
 from django.contrib import admin
 from .models import Menu
-from apps.branches.models import Branch
-from apps.storage.models import Category, Item, Ingredient
 
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('branch', 'category', 'item', 'ingredient')
 
 admin.site.register(Menu, MenuAdmin)
-admin.site.register(Branch)
-admin.site.register(Category)
-admin.site.register(Item)
-admin.site.register(Ingredient)
